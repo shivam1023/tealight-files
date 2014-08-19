2c51a12af7086afcc5541ff155590d4a624a8127
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-19 11:59:04 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -7,5 +7,11 @@ def polygon(edges, size, times):
   for i in range(0, edges):
       move(size)
       turn(angle)
+  for i2 in range(0,times):  
+    move(size)
+      turn(angle)
+    
+    
+    
     
 polygon(4,30,8)
