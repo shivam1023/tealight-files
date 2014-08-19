9bca0e8f3ce896a69266c37da00299b8203dc3be
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-19 14:40:05 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -6,13 +6,5 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-for i in range(0,7):
- move()
-turn(1)  
-for i in range(0,2):
- move()
-turn(-1)
-for i in range(0,5):
- move()
-turn(1) 
+
 
