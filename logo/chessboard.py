c5b749af9ace60f39b835a71a717fc3d487aa3d1
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-19 11:58:12 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -3,7 +3,7 @@ from tealight.logo import move, turn
 def polygon(edges, size, times):
   angle = 360.0 / edges
   
-  for i2 in range(0,times):
+
   for i in range(0, edges):
       move(size)
       turn(angle)
