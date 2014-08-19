9a93b28478cb95b48428ede5228cec5029923550
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-19 12:18:13 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -12,8 +12,8 @@ def square(edges, size):
     
 for n2 in range(0,8):
   turn(angle)
- for n in range(0,8):
-  square(4,30)
-  move (30)
+   for n in range(0,8):
+    square(4,30)
+    move (30)
   
   
