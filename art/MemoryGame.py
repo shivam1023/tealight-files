4c5d83852860a7fa414ae50ab4b6b23c0f6c3378
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-20 12:26:10 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -1,5 +1,4 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
-from tealight.art import array
 from tealight.art import (screen_width, screen_height)
 
 intarray = array(48)
