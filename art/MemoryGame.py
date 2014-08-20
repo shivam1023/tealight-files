843b08f17970a7ef34db533fcf9f2cbc52659e26
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-20 12:21:27 +0100

    Closing art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -1,1 +1,20 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import (screen_width, screen_height)
+
+print "This is art mode!"
+
+print screen_width
+print screen_height
+
+background("paper.jpg")
+image(200,200,"misc/Card.png")
+
+
+
+lastx = None
+lasty = None
+
+
+  
+  
