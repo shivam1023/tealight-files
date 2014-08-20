5d6a62760e9497bfcf467b29e600daf2895b5aaf
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-20 11:46:58 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -13,16 +13,16 @@ moves=0
    
 while True: 
   move()
-  moves+=1
+  moves=moves+1
   if left_side()!="wall":
     turn(-1)
     move()
-    moves+=1
+    moves=moves+1
     continue
   if touch()=="wall":
     turn(1)
     move()
-    moves+=1
+    moves=moves+1
     continue
   print moves  
     
