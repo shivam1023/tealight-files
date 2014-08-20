4722f843849b4ac91dfea2532314eb875ad9eee1
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-20 12:23:19 +0100

    Closing art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -2,19 +2,28 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import (screen_width, screen_height)
 
-print "This is art mode!"
-
 print screen_width
 print screen_height
 
 background("paper.jpg")
 image(200,200,"misc/Card.png")
 
+lastx = 0
+lasty = 0
 
 
-lastx = None
-lasty = None
-
-
+def handle_mousedown(x,y):
+  global lastx, lasty
   
+  lastx = x
+  lasty = y
+
+def handle_mousemove(x,y,button):
+  global lastx, lasty
+  
+  if button == "left":
+
+    line(lastx, lasty, x, y)
+    lastx = x
+    lasty = y
   
