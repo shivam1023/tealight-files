2a56d39ef666d9f0ccb5fc105d78a5bf49af159a
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-20 11:55:10 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -7,22 +7,16 @@ from tealight.robot import (move,
                             right_side)
 
 
-
-
-moves=0
    
 while True: 
   move()
-  moves=moves+1
   if left_side()!="wall":
-    turn(-2)
+    turn(-1)
     move()
-    moves=moves+1
     continue
   if touch()=="wall":
-    turn(2)
+    turn(1)
     move()
-    moves=moves+1
     continue
 
     
