7429e52a0078c228ac46214c01bac453bc358f76
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-21 15:35:20 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -62,20 +62,22 @@ def refreshCards(v):
   y = 150
   for i in range(0,48):
     if i==v:
-      image(x,y,deck[v].name2)
-      if x < 600:
-       x = x + 100
-      else:
-       x = 150
-       y = y + 100
-        
+      if deck[i].turnedOver == False: 
+       image(x,y,deck[v].name2)
+       if x < 600:
+        x = x + 100
+       else:
+        x = 150
+        y = y + 100
+         
     elif i == temp:
-      image(x,y,deck[temp].name2)
-      if x < 600:
-       x = x + 100
-      else:
-       x = 150
-       y = y + 100
+      if deck[i].turnedOver == False: 
+       image(x,y,deck[temp].name2)
+       if x < 600:
+        x = x + 100
+       else:
+        x = 150
+        y = y + 100
         
     else:
       
@@ -138,7 +140,8 @@ def handle_mousedown(x,y,button):
         refreshCards(v)
       
         if deck[temp].name2==deck[v].name2:
-          
+          deck[temp].turnedOver=True
+          deck[v].turnedOver=True
           
           score=score+1
           print score
