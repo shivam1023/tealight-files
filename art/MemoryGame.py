208c53897240201776d8a07770050d192bc5c5eb
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-21 15:30:00 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -17,6 +17,7 @@ global animals
 animals = ["animals/Bear.png", "animals/Bear.png", "animals/Cat.png", "animals/Cat.png", "animals/Dog.png", "animals/Dog.png", "animals/Elephant.png", "animals/Elephant.png", "animals/Frog.png", "animals/Frog.png", "animals/Horse.png", "animals/Horse.png", "animals/Ladybird.png", "animals/Ladybird.png", "animals/Lion.png", "animals/Lion.png", "animals/Lobster.png", "animals/Lobster.png", "animals/Penguin.png", "animals/Penguin.png", "animals/Puffin.png", "animals/Puffin.png", "animals/Seagull.png", "animals/Seagull.png", "animals/Seal.png", "animals/Seal.png", "animals/Sheep.png", "animals/Sheep.png", "animals/Swan.png", "animals/Swan.png", "animals/Tiger.png", "animals/Tiger.png", "animals/Ant.png", "animals/Ant.png", "animals/Butterfly.png", "animals/Butterfly.png", "animals/Diplodocus.png", "animals/Diplodocus.png", "animals/Dolphin.png", "animals/Dolphin.png", "animals/Fish1.png", "animals/Fish1.png", "animals/Stegosaurus.png", "animals/Stegosaurus.png", "animals/Fish2.png", "animals/Fish2.png", "animals/Pterodactyl.png", "animals/Pterodactyl.png"] 
 global deck
 deck = [Card() for i in range(48)]
+foundCards=[i for i in range(48)]
 
 x=0
 y=0
@@ -140,6 +141,7 @@ def handle_mousedown(x,y,button):
           cards_clicked=0
           temp=None
           v=None
+          
         else:
           
           cards_clicked=0
@@ -147,29 +149,7 @@ def handle_mousedown(x,y,button):
           v=None
      
     
-    #elif cards_clicked == 2:
-      
-     #  refreshCards(v)
-  
-      # if temp == v:  #possibly need new variable or to reference deck[]
-       # if deck[temp].name2==deck[v].name2:
-        # score = score + 1
-         #print score 
-  #       cards_clicked = 0
-   #      temp=None
-    #     v=None 
-     #   else:
-      #   temp=None
-       #  v=None 
-        # cards_clicked=0
-         #placeCards()
-          
-     
-      
-     
-    
-      #flip cards back over
-   
+ 
 
 
   
