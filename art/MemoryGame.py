3f443feebd6a96d5827d54aadfedd61a9a2c44ad
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-21 10:48:01 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -1,21 +1,25 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
 
-
-
-  
-
-print screen_width
-print screen_height
-
 background("paper.jpg")
 image(200,200,"misc/Card.png")
 
+
 listOfCards=[i for i in range(6)]+[i for i in range(8)]
  
+
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
 lastx = 0
 lasty = 0
-
 
 def handle_mousedown(x,y):
   global lastx, lasty
