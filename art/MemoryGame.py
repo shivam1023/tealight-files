0e5f9198b83e6330b713c39a7f0a084251ea4427
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-21 12:16:06 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -1,6 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
 import random
+from random import shuffle
 
 background("paper.jpg")
 
@@ -12,8 +13,7 @@ class Card:
     self.turnedOver=False
     
 global animals
-animals=["animals/Ant.png","animals/Ant.png","animals/Bear.png","animals/Bear.png"]
-
+animals = ["animals/Bear.png", "animals/Bear.png", "animals/Cat.png", "animals/Cat.png", "animals/Dog.png", "animals/Dog.png", "animals/Elephant.png", "animals/Elephant.png", "animals/Frog.png", "animals/Frog.png", "animals/Horse.png", "animals/Horse.png", "animals/Ladybird.png", "animals/Ladybird.png", "animals/Lion.png", "animals/Lion.png", "animals/Lobster.png", "animals/Lobster.png", "animals/Penguin.png", "animals/Penguin.png", "animals/Puffin.png", "animals/Puffin.png", "animals/Seagull.png", "animals/Seagull.png", "animals/Seal.png", "animals/Seal.png", "animals/Sheep.png", "animals/Sheep.png", "animals/Swan.png", "animals/Swan.png", "animals/Tiger.png", "animals/Tiger.png", "animals/Ant.png", "animals/Ant.png", "animals/Butterfly.png", "animals/Butterfly.png", "animals/Diplodocus.png", "animals/Diplodocus.png", "animals/Dolphin.png", "animals/Dolphin.png", "animals/Fish1.png", "animals/Fish1.png", "animals/Stegosaurus.png", "animals/Stegosaurus.png", "animals/Fish2.png", "animals/Fish2.png", "animals/Pterodactyl.png", "animals/Pterodactyl.png"] 
 global deck
 deck = [Card() for i in range(4)]
    
@@ -28,7 +28,6 @@ def shuffleCards():
 
 #write function to assign images to Card objects
 def assignImages():
-  
   for i in range(0,3):
      deck[i].name2=animals[i]
   
