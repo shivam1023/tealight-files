a3267aed05722d9d789dfcda8a9671d8286602cc
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-21 10:50:25 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -1,5 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
+import random
 
 background("paper.jpg")
 image(200,200,"misc/Card.png")
