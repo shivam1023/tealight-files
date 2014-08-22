3783acfdcae799a77a28c62999bd878f88c4da14
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-22 10:04:14 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -181,7 +181,7 @@ def handle_mousedown(x,y,button):
           
     if score == 24:
       color("black")
-      box(240,380, 500, 500)
+      box(240,380, 400, 300)
       image(275, 450, "http://i.imgur.com/eSSXfA8.png")
       
     else:
