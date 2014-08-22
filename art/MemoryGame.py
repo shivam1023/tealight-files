f9275778a510356eaf1ad7c3a7460ae4bcc6f6b0
shivam1023 <shivam1023@hotmail.co.uk>
2014-08-22 09:23:09 +0100

    Running art/MemoryGame.py

--- art/MemoryGame.py
+++ art/MemoryGame.py
@@ -178,10 +178,10 @@ def handle_mousedown(x,y,button):
           
           
     if score == 24:
-      text(300,50,"You win, go away!")
+      text(430,973,"You win, go away!")
       
     else:
-      text(300,50,"You have scored, %d" %score)
+      text(430,973,"You have scored, %d" %score)
      
     
  
